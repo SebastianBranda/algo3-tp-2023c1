8696099679f46f223775e98b031700c4ad9f8c5e
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-04-15 19:05:31 -0300

    Creación Abstract Class Actividad
    Se crea la clase abstracta Actividad

---
+++ src/main/java/Actividad.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+abstract class Actividad {
+    protected String titulo;
+    protected String descripcion;
+    protected ArrayList alarmas;
+    protected Boolean esActividadDelDia;
+
+}
