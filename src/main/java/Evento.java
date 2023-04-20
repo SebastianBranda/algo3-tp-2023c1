1fa268ddaa69baa917a5e36fe1c6b4f45ff76e51
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-04-20 18:37:59 -0300

    Updating classes and sub-classes
    Updating classes:
    Actividad and Calendario.
    
    Updating sub-classes:
    Evento and Tarea

--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -1,15 +1,17 @@
 import java.time.LocalDateTime;
-import java.util.Dictionary;
+import java.util.HashMap;
 
 public class Evento extends Actividad {
     protected Frecuencia frecuencia;
+    protected Boolean esRepetible;
+    @Override
+    protected void modificar(){};
     public Evento(){
         super();
     }
-
-    public Evento(String titulo, String descripcion, LocalDateTime fechaHora, LocalDateTime fechaHoraFin, Dictionary<String, Alarma> diccionarioAlarmas, Boolean esActividadDelDia, Boolean esRepetible, Frecuencia frecuencia){
-        super(titulo, descripcion, fechaHora, fechaHoraFin, diccionarioAlarmas, esActividadDelDia, esRepetible);
+    public Evento(String titulo, String descripcion, LocalDateTime fechaHora, LocalDateTime fechaHoraFin, HashMap<String, Alarma> alarmas, Boolean esActividadDelDia, Boolean esRepetible, Frecuencia frecuencia){
+        super(titulo, descripcion, fechaHora, fechaHoraFin, alarmas, esActividadDelDia);
         this.frecuencia = frecuencia;
+        this.esRepetible = esRepetible;
     }
-    //public modificar(){ Discutur implementación}
 }
