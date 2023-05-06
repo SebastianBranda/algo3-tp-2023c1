c6b42a7f813e6ea6e46c8c174c1c7c1be15b50f7
SebastianBranda <sebastianbranda@gmail.com>
2023-05-06 17:38:14 -0300

    evitar repeticiones en dias de semana a repetir

--- src/main/java/FrecuenciaSemanal.java
+++ src/main/java/FrecuenciaSemanal.java
@@ -2,20 +2,25 @@ import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class FrecuenciaSemanal extends Frecuencia{
-    private ArrayList<DayOfWeek> diasRepeticion= new ArrayList<>();
+//    private ArrayList<DayOfWeek> diasRepeticion= new ArrayList<>();
+    private Set<DayOfWeek> diasRepeticion;
 
     public FrecuenciaSemanal(LocalDateTime fecha, DayOfWeek [] dias){
         this.fechaInicial = fecha;
         this.cantidadRepeticiones = dias.length;
         this.esDuracionInfinita = false;
+        this.diasRepeticion = new TreeSet<>();
         this.agregarDiasSemana(dias);
     }
     public FrecuenciaSemanal(LocalDateTime fecha, boolean esDuracionInfinita, DayOfWeek [] dias){
         this.fechaInicial = fecha;
         this.cantidadRepeticiones = 0;
         this.esDuracionInfinita = esDuracionInfinita;
+        this.diasRepeticion = new TreeSet<>();
         this.agregarDiasSemana(dias);
     }
     public FrecuenciaSemanal(LocalDateTime fechaInicial, LocalDateTime fechaFinal, DayOfWeek [] dias){
@@ -23,12 +28,14 @@ public class FrecuenciaSemanal extends Frecuencia{
         long cantDias = fechaInicial.until(fechaFinal, ChronoUnit.DAYS);
         this.cantidadRepeticiones = ((int) (cantDias/7)) * dias.length;
         this.esDuracionInfinita = false;
+        this.diasRepeticion = new TreeSet<>();
         this.agregarDiasSemana(dias);
     }
     public FrecuenciaSemanal(LocalDateTime fechaInicial, int cantRepeticiones, DayOfWeek [] dias){
         this.fechaInicial = fechaInicial;
         this.cantidadRepeticiones = cantRepeticiones;
         this.esDuracionInfinita = false;
+        this.diasRepeticion = new TreeSet<>();
         this.agregarDiasSemana(dias);
     }
     private void agregarDiasSemana(DayOfWeek [] dias){
