b6696879f4fa7fda5b78de8be918f627d2b99db2
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-05-31 18:42:36 -0300

    Update CalendarioPersistenciaTest.java
    changes to solve conflict to allow reviewed pullreq to update main with official etapa2

--- src/test/java/CalendarioPersistenciaTest.java
+++ src/test/java/CalendarioPersistenciaTest.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
 public class CalendarioPersistenciaTest {
 
     @Test
-    public void persistenciaTest() {
+    public void persistenciaTestEvento() {
         Calendario calendario = new Calendario();
         LocalDateTime fecha = LocalDateTime.of(2023, 05, 01, 01, 01);
         FrecuenciaDiaria freq = new FrecuenciaDiaria(fecha);
         Evento evento = new Evento("Test Evento", "Esto es un evento", fecha, fecha.plusMinutes(30), false, freq, TipoFrecuencia.DIARIA, false);
         calendario.agregarEvento(evento);
-        String nombreArchivo = "test.txt";
+        String nombreArchivo = "testEvento.txt";
 
         calendario.guardarActividades(nombreArchivo);
-         ArrayList<Actividad> actividadesCargadas = calendario.cargarActividades(nombreArchivo);
+        ArrayList<Actividad> actividadesCargadas = calendario.cargarActividades(nombreArchivo);
         Actividad eventoCargado = actividadesCargadas.get(0);
 
         Assert.assertEquals(evento.titulo, eventoCargado.titulo);
