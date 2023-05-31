a0a31e8b9eff0d69d6521fa64647a6240032ecb7
Jsf-MK-ian <93227973+Jsf-MK-ian@users.noreply.github.com>
2023-05-31 15:27:53 -0300

    Agregar TareaTest
    Falta completar

---
+++ src/test/java/TareaTest.java
@@ -0,0 +1,39 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class TareaTest {
+    @Test
+    public void listaDeAlarmasVacia(){
+        LocalDateTime fecha = LocalDateTime.of(2023, 05, 01, 01, 01);
+        Tarea t = new Tarea("t",
+                "d",
+                fecha,
+                LocalDateTime.of(2050, 01, 01, 01, 01),
+                false,
+                false
+                );
+        Assert.assertEquals(0, t.alarmas.size());
+    }
+
+    @Test
+    public void nuevaAlarmaAgregaAListaDeAlarmas(){
+        LocalDateTime fecha = LocalDateTime.of(2023, 05, 01, 01, 01);
+        Tarea t = new Tarea("t",
+                "d",
+                fecha,
+                LocalDateTime.of(2050, 01, 01, 01, 01),
+                false,
+                false
+        );
+        Alarma alarma= new AlarmaEmail(fecha);
+        t.agregarAlarma(alarma);
+        Assert.assertEquals(1, t.alarmas.size());
+    }
+
+    
+
+}
